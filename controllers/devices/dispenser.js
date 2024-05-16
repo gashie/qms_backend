d5e8bfde95ac0f3acfb78b6ede3202be06669c91
gashie <richardwilliam60@gmail.com>
2024-05-16 11:59:53 +0000

    template update

--- controllers/devices/dispenser.js
+++ controllers/devices/dispenser.js
@@ -8,7 +8,7 @@ const systemDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
 exports.CreateDispenserTemplate = asynHandler(async (req, res, next) => {
     // Assuming template_type is a variable containing the template type ("video" or "image")
-    let {template_type} = req.body
+    let { template_type } = req.body
     let payload = req.body;
 
     let background_image = req.files?.background_image
@@ -22,15 +22,15 @@ exports.CreateDispenserTemplate = asynHandler(async (req, res, next) => {
 
             } else {
                 // background_video is a video
-                let fileData =  await autoSaveFile(background_video,'dispenser','./uploads/videos/dispenser/',background_video.name,background_video.mimetype,'dispenser_template_setup');
-  
+                let fileData = await autoSaveFile(background_video, 'dispenser', './uploads/videos/dispenser/', background_video.name, background_video.mimetype, 'dispenser_template_setup');
+
                 payload.background_video = fileData.file_name
                 let results = await GlobalModel.Create(payload, 'dispenser_templates', '');
                 if (results.rowCount == 1) {
                     return sendResponse(res, 1, 200, "Record saved", [])
                 } else {
                     return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
-            
+
                 }
             }
         } else {
@@ -49,18 +49,18 @@ exports.CreateDispenserTemplate = asynHandler(async (req, res, next) => {
 
             if (background_image.mimetype && allowedImageTypes.includes(background_image.mimetype)) {
                 // background_image is an image file with a valid mimetype
-               
-              let fileData =  await autoSaveFile(background_image,'dispenser','./uploads/images/dispenser/',background_image.name,background_image.mimetype,'dispenser_template_setup');
-              payload.background_image = fileData.file_name
-              let results = await GlobalModel.Create(payload, 'dispenser_templates', '');
-              if (results.rowCount == 1) {
-                  return sendResponse(res, 1, 200, "Record saved", [])
-              } else {
-                  return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
-          
-              }
-          
-    
+
+                let fileData = await autoSaveFile(background_image, 'dispenser', './uploads/images/dispenser/', background_image.name, background_image.mimetype, 'dispenser_template_setup');
+                payload.background_image = fileData.file_name
+                let results = await GlobalModel.Create(payload, 'dispenser_templates', '');
+                if (results.rowCount == 1) {
+                    return sendResponse(res, 1, 200, "Record saved", [])
+                } else {
+                    return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
+
+                }
+
+
             } else if (background_image.match(imageUrlRegex)) {
                 // background_image is a valid image link
                 let results = await GlobalModel.Create(payload, 'dispenser_templates', '');
@@ -68,9 +68,9 @@ exports.CreateDispenserTemplate = asynHandler(async (req, res, next) => {
                     return sendResponse(res, 1, 200, "Record saved", [])
                 } else {
                     return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
-            
+
                 }
-            
+
             } else {
                 return sendResponse(res, 0, 200, "Sorry,kindly provide file or link to the image", [])
 
@@ -89,40 +89,40 @@ exports.CreateDispenserTemplate = asynHandler(async (req, res, next) => {
             let itemCount = background_image.length;
             let isDone = false
             for (const item of items) {
-                let fileData =  await autoSaveFile(item,'dispenser','./uploads/images/dispenser/',item.name,item.mimetype,'dispenser_template_setup');
-                let media_content ={}
-                media_content.content_type ='image'
-                media_content.assigned_to =req.body.assigned_to
+                let fileData = await autoSaveFile(item, 'dispenser', './uploads/images/dispenser/', item.name, item.mimetype, 'dispenser_template_setup');
+                let media_content = {}
+                media_content.content_type = 'image'
+                media_content.assigned_to = req.body.assigned_to
                 media_content.content_url = fileData.file_name
-                media_content.dispenser_template_id =results.rows[0].template_id
+                media_content.dispenser_template_id = results.rows[0].template_id
                 let content = await GlobalModel.Create(media_content, 'media_content', '');
                 // if (results.rowCount == 1) {
                 //     return sendResponse(res, 1, 200, "Record saved", [])
                 // } else {
                 //     return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
-            
+
                 // }
                 if (!--itemCount) {
                     isDone = true;
                     console.log(" => This is the last iteration...");
-        
+
                 } else {
                     console.log(" => Still saving data...");
-        
+
                 }
             }
             if (isDone) {
-                return sendResponse(res, 1, 200, `${items.length} new carousel images added to template with id ${results.rows[0].template_id}`, { template:results.rows[0].template_id })
+                return sendResponse(res, 1, 200, `${items.length} new carousel images added to template with id ${results.rows[0].template_id}`, { template: results.rows[0].template_id })
             }
         } else {
             return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
-    
+
         }
- 
+
     }
 
 
-   
+
 })
 
 exports.AssignTemplateToDispenser = asynHandler(async (req, res, next) => {
@@ -176,13 +176,122 @@ exports.UpdateDispenserTemplate = asynHandler(async (req, res, next) => {
     let payload = req.body;
     payload.updated_at = systemDate
 
-    const runupdate = await GlobalModel.Update(payload, 'dispenser_templates', 'template_id', payload.template_id)
-    if (runupdate.rowCount == 1) {
-        return sendResponse(res, 1, 200, "Record Updated", runupdate.rows[0])
+    const tableName = 'dispenser_templates';
+    const columnsToSelect = []; // Use string values for column names
+    const ServiceConditions = [
+        { column: 'template_id', operator: '=', value: payload?.template_id },
+    ];
+    let results = await GlobalModel.Finder(tableName, columnsToSelect, ServiceConditions)
+    if (results.rows.length == 0) {
+        return sendResponse(res, 0, 200, "Sorry, No Record Found", [])
+    }
+    //oldrecord
+    let background_image = req.files?.background_image
+    let background_video = req.files?.background_video
+    if (template_type === "video") {
+        // Check only background_video
+        if (background_video) {
+            if (!background_video.mimetype.startsWith("video")) {
+                // Handle case where background_video is not a video
+                return sendResponse(res, 0, 200, "Sorry,kindly provide video file", [])
+
+            } else {
+                // background_video is a video
+                let fileData = await autoSaveFile(background_video, 'dispenser', './uploads/videos/dispenser/', background_video.name, background_video.mimetype, 'dispenser_template_setup');
+                payload.background_video = fileData.file_name
+                const results = await GlobalModel.Update(payload, 'dispenser_templates', 'template_id', payload.template_id)
+                if (results.rowCount == 1) {
+                    return sendResponse(res, 1, 200, "Record saved", [])
+                } else {
+                    return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
+
+                }
+            }
+        } else {
+            return sendResponse(res, 0, 200, "Sorry,a video file is required for this operation", [])
+
+            // Handle case where background_video is not provided but required
+        }
+    } else if (template_type === "image") {
+        // Check only background_image
+        if (background_image) {
+            // Define an array of allowed image mimetypes or an array of image file extensions
+            const allowedImageTypes = ["image/jpeg", "image/png", "image/gif", /* Add more if needed */];
+
+            // Define a regular expression to match image links (URLs)
+            const imageUrlRegex = /^https?:\/\/.+\.(jpeg|jpg|png|gif)$/i;
+
+            if (background_image.mimetype && allowedImageTypes.includes(background_image.mimetype)) {
+                // background_image is an image file with a valid mimetype
+
+                let fileData = await autoSaveFile(background_image, 'dispenser', './uploads/images/dispenser/', background_image.name, background_image.mimetype, 'dispenser_template_setup');
+                payload.background_image = fileData.file_name
+                const results = await GlobalModel.Update(payload, 'dispenser_templates', 'template_id', payload.template_id)
+                if (results.rowCount == 1) {
+                    return sendResponse(res, 1, 200, "Record saved", [])
+                } else {
+                    return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
+
+                }
 
 
+            } else if (background_image.match(imageUrlRegex)) {
+                // background_image is a valid image link
+                let results = await GlobalModel.Create(payload, 'dispenser_templates', '');
+                if (results.rowCount == 1) {
+                    return sendResponse(res, 1, 200, "Record saved", [])
+                } else {
+                    return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
+
+                }
+
+            } else {
+                return sendResponse(res, 0, 200, "Sorry,kindly provide file or link to the image", [])
+
+                // Handle case where background_image is not a valid image or image link
+            }
+        } else {
+            // Handle case where background_image is not provided but required
+        }
     } else {
-        return sendResponse(res, 0, 200, "Update failed, please try later", [])
+        // Handle case where template_type is neither "video" nor "image"
+        return sendResponse(res, 0, 200, "Sorry, you can only update image and video", [])
+
+
+    }
+})
+exports.UpdateDispenserCarouselTemplate = asynHandler(async (req, res, next) => {
+    let payload = req.body;
+    payload.updated_at = systemDate
+    let content_id = payload.content_id
+
+    const tableName = 'media_content';
+    const columnsToSelect = []; // Use string values for column names
+    const ServiceConditions = [
+        { column: 'content_id', operator: '=', value: payload?.content_id },
+    ];
+    let temp_results = await GlobalModel.Finder(tableName, columnsToSelect, ServiceConditions)
+    if (temp_results.rows.length == 0) {
+        return sendResponse(res, 0, 200, "Sorry, No Record Found", [])
+    }
+    //oldrecord
+    let background_image = req.files?.background_image
+
+    // Handle case where template_type is neither "video" nor "image"
+
+    //-1 save template
+    let fileData = await autoSaveFile(background_image, 'dispenser', './uploads/images/dispenser/', background_image.name, background_image.mimetype, 'dispenser_template_update');
+    let media_content = {}
+    media_content.content_type = 'image'
+    media_content.assigned_to = req.body.assigned_to
+    media_content.content_url = fileData.file_name
+    media_content.dispenser_template_id = template_id
+    const results = await GlobalModel.Update(payload, 'media_content', 'contend_id', content_id)
+    if (results.rowCount == 1) {
+        return sendResponse(res, 1, 200, "Record saved", [])
+    } else {
+        return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
+
     }
 })
 exports.UpdateAssignedTemplate = asynHandler(async (req, res, next) => {
